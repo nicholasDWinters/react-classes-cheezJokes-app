9d4188b65a8ec265b73f38b6a1b1c4e6ad2f6ea5
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-06-02 10:42:49 -0500

    added local storage functionality

--- src/Joke.js
+++ src/Joke.js
@@ -1,27 +1,30 @@
 import React from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {
-  const upVote = () => vote(id, +1);
-  const downVote = () => vote(id, -1);
+class Joke extends React.Component {
+  upVote = () => this.props.vote(this.props.id, +1);
+  downVote = () => this.props.vote(this.props.id, -1);
 
-  return (
-    <div className="Joke">
-      <div className="Joke-votearea">
-        <button onClick={upVote}>
-          <i className="fas fa-thumbs-up" />
-        </button>
+  render() {
+    return (
+      <div className="Joke">
+        <div className="Joke-votearea">
+          <button onClick={this.upVote}>
+            <i className="fas fa-thumbs-up" />
+          </button>
 
-        <button onClick={downVote}>
-          <i className="fas fa-thumbs-down" />
-        </button>
+          <button onClick={this.downVote}>
+            <i className="fas fa-thumbs-down" />
+          </button>
 
-        {votes}
+          {this.props.votes}
+        </div>
+
+        <div className="Joke-text">{this.props.text}</div>
       </div>
+    );
+  }
 
-      <div className="Joke-text">{text}</div>
-    </div>
-  );
 }
 
 export default Joke;
--- src/JokeList.js
+++ src/JokeList.js
@@ -1,73 +1,89 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import Joke from "./Joke";
 import "./JokeList.css";
 
-function JokeList({ numJokesToGet = 10 }) {
-  const [jokes, setJokes] = useState([]);
+
+class JokeList extends React.Component {
+  state = {
+    jokes: []
+  }
 
   /* get jokes if there are no jokes */
 
-  useEffect(function() {
-    async function getJokes() {
-      let j = [...jokes];
-      let seenJokes = new Set();
-      try {
-        while (j.length < numJokesToGet) {
-          let res = await axios.get("https://icanhazdadjoke.com", {
-            headers: { Accept: "application/json" }
-          });
-          let { status, ...jokeObj } = res.data;
-  
-          if (!seenJokes.has(jokeObj.id)) {
-            seenJokes.add(jokeObj.id);
-            j.push({ ...jokeObj, votes: 0 });
-          } else {
-            console.error("duplicate found!");
-          }
-        }
-        setJokes(j);
-      } catch (e) {
-        console.log(e);
-      }
-    }
 
-    if (jokes.length === 0) getJokes();
-  }, [jokes, numJokesToGet]);
+  async getJokes() {
+
+    let j = [...this.state.jokes];
+    let seenJokes = new Set();
+    try {
+      while (j.length < this.props.numJokesToGet) {
+        let res = await axios.get("https://icanhazdadjoke.com", {
+          headers: { Accept: "application/json" }
+        });
+        let { status, ...jokeObj } = res.data;
+
+        if (!seenJokes.has(jokeObj.id)) {
+          seenJokes.add(jokeObj.id);
+          j.push({ ...jokeObj, votes: 0 });
+        } else {
+          console.error("duplicate found!");
+        }
+      }
+      localStorage.setItem('jokes', JSON.stringify(j));
+      return this.setState({ jokes: JSON.parse(localStorage.getItem('jokes')) })
+      // return this.setState({ jokes: j });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async componentDidMount() {
+    if (localStorage.jokes) return this.setState({ jokes: JSON.parse(localStorage.getItem('jokes')) })
+    if (this.state.jokes.length === 0) this.getJokes();
+  }
+
+  async componentDidUpdate() {
+    if (this.state.jokes.length === 0) return this.getJokes();
+    localStorage.setItem('jokes', JSON.stringify(this.state.jokes));
+  }
+
 
   /* empty joke list and then call getJokes */
 
-  function generateNewJokes() {
-    setJokes([]);
+  generateNewJokes = () => {
+    localStorage.removeItem('jokes');
+    this.setState({ jokes: [] });
   }
 
   /* change vote for this id by delta (+1 or -1) */
 
-  function vote(id, delta) {
-    setJokes(allJokes =>
-      allJokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
-    );
+  vote = (id, delta) => {
+    this.setState({ jokes: this.state.jokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j)) })
+    localStorage.setItem('jokes', JSON.stringify(this.state.jokes));
   }
 
   /* render: either loading spinner or list of sorted jokes. */
 
-  if (jokes.length) {
-    let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
-  
-    return (
-      <div className="JokeList">
-        <button className="JokeList-getmore" onClick={generateNewJokes}>
-          Get New Jokes
+  render() {
+    if (this.state.jokes.length) {
+      let sortedJokes = [...this.state.jokes].sort((a, b) => b.votes - a.votes);
+      return (
+        <div className="JokeList">
+          <button className="JokeList-getmore" onClick={this.generateNewJokes}>
+            Get New Jokes
         </button>
-  
-        {sortedJokes.map(j => (
-          <Joke text={j.joke} key={j.id} id={j.id} votes={j.votes} vote={vote} />
-        ))}
-      </div>
-    );
-  }
 
-  return null;
+          {sortedJokes.map(j => (
+            <Joke text={j.joke} key={j.id} id={j.id} votes={j.votes} vote={this.vote} />
+          ))}
+        </div>
+      );
+    } else {
+      return <h1 className='loading'>Loading...</h1>;
+    }
+
+  }
 
 }
 
